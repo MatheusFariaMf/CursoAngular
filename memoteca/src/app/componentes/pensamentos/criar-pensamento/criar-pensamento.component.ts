99bca957f3fca38c4f15846126a4e628258613de
MatheusFariaMf <matheus.mfd@hotmail.com>
2025-03-20 22:19:15 -0300

    Finalização do primeiro curso: fim do CRUD
    Na aplicação se tornou possível excluir e editar os pensamentos existentes.

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -7,24 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  pensamento = {
-    id: '1',
-    conteudo: 'Aprendendo Angular teste',
-    autoria: 'Dev',
+  pensamento : Pensamento = {
+    conteudo: '',
+    autoria: '',
     modelo: 'modelo2'
   }
 
-  constructor() { }
+  constructor(private service: PensamentoService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   criarPensamento(){
-    alert("Novo Pensamento Criado");
+    this.service.criar(this.pensamento).subscribe();
+    this.router.navigate(['/listarPensamento']);
   }
 
   cancelarPensamento(){
-    alert("Pensamento Cancelado");
+    this.router.navigate(['/listarPensamento']);
   }
 
 }
