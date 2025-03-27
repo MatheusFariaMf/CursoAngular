8cfd1ef89b47148405a3fa8c56843ad83011b80c
MatheusFariaMf <matheus.mfd@hotmail.com>
2025-03-26 21:52:52 -0300

    InsercaoDeValidacoes
    Os formulários de inserção de pensamento e de edição agora possuem validações dos seus campos de pensamento e autor.

--- memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-pensamento',
@@ -10,34 +11,66 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class EditarPensamentoComponent implements OnInit {
 
-  pensamento: Pensamento = {
-    id: 0,
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
+  formulario!: FormGroup;
 
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      id: [0, ''],
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        EditarPensamentoComponent.apenasLetrasMinusculas
+      ])],
+      modelo: ['modelo1']
+    })
+
     const id = this.route.snapshot.paramMap.get('id')
     this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
-      this.pensamento = pensamento;
+      this.formulario.setValue({
+        id: pensamento.id,
+        conteudo: pensamento.conteudo,
+        autoria: pensamento.autoria,
+        modelo: pensamento.modelo
+      });
     })
   }
 
   editarPensamento(){
-    this.service.editar(this.pensamento).subscribe(() => {
+    this.service.editar(this.formulario.value).subscribe(() => {
       this.router.navigate(['/listarPensamento'])
     })
   }
 
   cancelarPensamento(){
     this.router.navigate(['/listarPensamento'])
+  }
+
+  habilitarBotao(): string {
+    if(this.formulario.valid){
+      return 'botao'
+    }
+    return 'botao__desabilitado'
+  }
+
+  static apenasLetrasMinusculas(control: AbstractControl): ValidationErrors | null {
+    console.log(control.value)
+    console.log(control.value.toLowerCase())
+    if(control.value && control.value === control.value.toLowerCase()){
+      return null;
+    }
+
+    return { apenasLetrasMinusculas: true }
   }
 
 }
